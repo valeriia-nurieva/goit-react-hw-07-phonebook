a564092c9e685b874ef9bdbf694a9e103fc8bd0f
valeriia-nurieva <valerka.0906@gmail.com>
2023-01-12 17:48:30 +0200

    update app and loading

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,17 +1,26 @@
+import { useSelector } from 'react-redux';
 import Form from '../Form';
 import ContactList from '../ContactList';
 import Filter from '../Filter';
 import Layout from '../Layout';
 import GlobalStyle from '../GlobalStyle';
 import Title from './App.styled';
+import { Loader } from "components/Loader";
+import { getError, getIsLoading } from 'redux/selectors';
+
 
 const App = () => {
+    const isLoading = useSelector(getIsLoading);
+  const error = useSelector(getError);
   return (
     <Layout>
+      
       <Title>Phonebook</Title>
       <Form />
       <Title as="h2">Contacts</Title>
       <Filter />
+      {isLoading && !error && <Loader/>}
+      {error && <p>{error}</p>}
       <ContactList />
       <GlobalStyle />
     </Layout>
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,15 +1,12 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from 'react-redux';
 import ContactItem from 'components/ContactItem';
-import { Loader } from "components/Loader";
 import { List, Item } from './ContactList.styled';
 import { fetchContacts } from "redux/operations";
-import { getContacts, getError, getIsLoading, getFilter } from 'redux/selectors';
+import { getContacts, getFilter } from 'redux/selectors';
 
 const ContactList = () => {
   const contacts = useSelector(getContacts);
-  const isLoading = useSelector(getIsLoading);
-  const error = useSelector(getError);
   const filter = useSelector(getFilter);
   const dispatch = useDispatch();
 
@@ -26,8 +23,6 @@ const ContactList = () => {
 
   return (
     <List>
-      {isLoading && !error && <Loader/>}
-      {error && <p>{error}</p>}
       {contacts.length > 0 && getFilteredContacts().map(({ id, name, phone }) => {
         return (
           <Item key={id}>
