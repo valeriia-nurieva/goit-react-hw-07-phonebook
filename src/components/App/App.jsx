3eed2198167799d6a72b143c928b50444a523bda
valeriia-nurieva <valerka.0906@gmail.com>
2023-01-12 18:06:29 +0200

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import Form from '../Form';
 import ContactList from '../ContactList';
@@ -6,27 +6,25 @@ import Filter from '../Filter';
 import Layout from '../Layout';
 import GlobalStyle from '../GlobalStyle';
 import Title from './App.styled';
-import { Loader } from "components/Loader";
+import { Loader } from 'components/Loader';
 import { getError, getIsLoading } from 'redux/selectors';
-import { fetchContacts } from "redux/operations";
-
+import { fetchContacts } from 'redux/operations';
 
 const App = () => {
-    const isLoading = useSelector(getIsLoading);
+  const isLoading = useSelector(getIsLoading);
   const error = useSelector(getError);
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(fetchContacts());
-  }, [dispatch])
+  }, [dispatch]);
   return (
     <Layout>
-      
       <Title>Phonebook</Title>
       <Form />
       <Title as="h2">Contacts</Title>
       <Filter />
-      {isLoading && !error && <Loader/>}
+      {isLoading && !error && <Loader />}
       {error && <p>{error}</p>}
       <ContactList />
       <GlobalStyle />
