28519474f8d1613b73cfceb12164aecee69b2e65
valeriia-nurieva <valerka.0906@gmail.com>
2023-01-13 21:08:56 +0200

    Update contactsSlice.js

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -18,6 +18,21 @@ const handleFullfilled = state => {
   state.error = null;
 };
 
+const fetchContactsFulfilledReducer = (state, action) => {
+  state.items = action.payload;
+};
+
+const addContactFulfilledReducer = (state, action) => {
+  state.items.push(action.payload);
+};
+
+const deleteContactFulfilledReducer = (state, action) => {
+  const index = state.items.findIndex(
+    task => task.id === action.payload.id
+  );
+  state.items.splice(index, 1);
+};
+
 const contactsSlice = createSlice({
   name: 'contacts',
   initialState: {
@@ -27,18 +42,9 @@ const contactsSlice = createSlice({
   },
   extraReducers: builder => {
     builder
-      .addCase(fetchContacts.fulfilled, (state, action) => {
-        state.items = action.payload;
-      })
-      .addCase(addContact.fulfilled, (state, action) => {
-        state.items.push(action.payload);
-      })
-      .addCase(deleteContact.fulfilled, (state, action) => {
-        const index = state.items.findIndex(
-          task => task.id === action.payload.id
-        );
-        state.items.splice(index, 1);
-      })
+      .addCase(fetchContacts.fulfilled, fetchContactsFulfilledReducer)
+      .addCase(addContact.fulfilled, addContactFulfilledReducer)
+      .addCase(deleteContact.fulfilled, deleteContactFulfilledReducer)
       .addMatcher(getActions('pending'), handlePending)
       .addMatcher(getActions('rejected'), handleRejected)
       .addMatcher(getActions('fulfilled'), handleFullfilled);
